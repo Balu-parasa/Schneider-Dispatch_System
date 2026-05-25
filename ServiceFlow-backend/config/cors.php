a1f252c1722d06308a157ef7561f994141b572fa
balu parasa <baluparasa@gmail.com>
2026-05-25 16:13:05 +0530

    Update CORS to allow all Vercel domains

--- ServiceFlow-backend/config/cors.php
+++ ServiceFlow-backend/config/cors.php
@@ -8,7 +8,7 @@ return [
         'http://127.0.0.1:3000',
         'http://localhost:3000',
     ]),
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['/.*\.vercel\.app$/'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
